52aae9080f0a0afe2579d021949518a798d05452
Sai Santosh Raju <rajusantosh77@gmail.com>
2022-06-29 10:03:56 +0100

    Update hello_world.c

--- exercises/practice/hello-world/hello_world.c
+++ exercises/practice/hello-world/hello_world.c
@@ -10,5 +10,6 @@ const char *hello(void)
 {
    // To fix this function, change the return statement here to instead return
    // a string equivalent to the string expected by the failing test.
+   printf("Hello world!");
    return NULL;
 }
